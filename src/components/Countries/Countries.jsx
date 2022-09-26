1f23d99c72a47c11f0fc49de0d03b0f23f27a633
abvictor <victor.amaralb@hotmail.com>
2022-09-26 20:01:18 -0300

    feat: responsividade

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import {Link} from 'react-router-dom'
+
 import useFetchCountries from "../../hooks/useFetchCountries";
 
 import styles from "./countries.module.css";
@@ -7,48 +7,46 @@ import styles from "./countries.module.css";
 const Countries = () => {
   const { countries, loading, error } = useFetchCountries();
   const [search, setSearch] = useState('')
-  const [region, setSelectRegion] = useState([])
+  const [selectRegion, setSelectRegion] = useState([])
 
   if (loading) return <h3 className={styles.loading}>Carregando...</h3>;
   if (error) console.log(error);
 
 
-  const filteredCountries = search.length > 0
-  ? countries.filter((country)=> country.name.toLowerCase().includes(search.toLowerCase()))
+  const filteredCountries = search.length > 0 ? countries.filter((country)=> country.name.toLowerCase().includes(search.toLowerCase()))
   : []
 
-  function handleCountryByRegion(e){
 
-    const selected = countries.filter((country)=>country.region === e.target.value);
-    
+  function handleFilterCountriesByRegion(e) {
 
-    let filteredSelect = selected.filter((este, i) => selected.indexOf(este) === i);
-    
-    console.log(filteredSelect)
-    setSelectRegion(filteredSelect)
+    const region = countries.filter(item => item.region === e.target.value);
+
+    setSelectRegion(region)
+   
   }
+  const regions = countries.map(country => country.region)
+  const filteredRegions = regions.filter((country, index) => regions.indexOf(country) === index)
 
 
   return (
     <main className={styles.main_container}>
 
       <div className={styles.filters}>
-      <input placeholder="Search for a country..." className={styles.input_search} value={search} onChange={e => setSearch(e.target.value)}  />
+      <input placeholder="Search for a country..." className={styles.input_search}  onChange={e =>setSearch(e.target.value)}/>
       
 
-      <select className={styles.select_filter} onChange={handleCountryByRegion}>
-     {countries.map((country)=>(
-      
-     <option key={country.name} value={country.region}>{country.region}</option>
-     ))}
+      <select className={styles.select_filter} onChange={handleFilterCountriesByRegion}>
+         <option value="">Search by region</option>
+         {filteredRegions.map((country)=>(
+            <option key={country}>{country}</option>
+          ))}
         </select>
       </div>
 
-      {search.length > 0 ? (
-
+      {search.length > 0  ? (
         <div className={styles.countries_container}>
           {filteredCountries.map((country) => (
-            <Link to={`/country/${country.name}`} key={country.name}>
+            
               <div className={styles.card_styles}>
                 <div className={styles.items}>
                 <img src={country.flag} alt="Country flag" />
@@ -56,17 +54,17 @@ const Countries = () => {
                 <div className={styles.country_info}>
                 <p><strong>Region:</strong> {country.region}</p>
                 <p><strong>Population:</strong> {country.population}</p>
-                <p><strong>Population:</strong> {country.capital}</p>
+                <p><strong>Capital:</strong> {country.capital}</p>
                 </div>
                 </div>  
               </div>
-            </Link>
+           
           ))}
         </div>
       ) : (
         <div className={styles.countries_container}>
           {countries.map((country) => (
-            <Link to={`/country/${country.name}`} key={country.name}>
+           
               <div className={styles.card_styles}>
                 <div className={styles.items}>
                 <img src={country.flag} alt="Country flag" />
@@ -74,16 +72,15 @@ const Countries = () => {
                 <div className={styles.country_info}>
                 <p><strong>Region:</strong> {country.region}</p>
                 <p><strong>Population:</strong> {country.population}</p>
-                <p><strong>Population:</strong> {country.capital}</p>
+                <p><strong>Capital:</strong> {country.capital}</p>
                 </div>
                 </div>  
               </div>
-            </Link>
           ))}
         </div>
       )}
-    
-    </main>
+
+      </main>
   );
 };
 
