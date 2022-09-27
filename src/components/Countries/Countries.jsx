4f5aefffce13dd21e751b3957ae697dda7ac34fa
abvictor <victor.amaralb@hotmail.com>
2022-09-27 14:43:44 -0300

    improvement: select filter

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 
 import useFetchCountries from "../../hooks/useFetchCountries";
 
@@ -6,11 +6,15 @@ import styles from "./countries.module.css";
 
 const Countries = () => {
   const { countries, loading, error } = useFetchCountries();
-  const [search, setSearch] = useState('')
-  const [selectRegion, setSelectRegion] = useState([])
+  const [search, setSearch] = useState("");
+  const [region, setRegion] = useState([])
 
-  if (loading) return <h3 className={styles.loading}>Carregando...</h3>;
-  if (error) console.log(error);
+ 
+
+  useEffect(() => {
+    
+  }, [search]);
+
 
 
   const filteredCountries = search.length > 0 ? countries.filter((country)=> country.name.toLowerCase().includes(search.toLowerCase()))
@@ -21,66 +25,104 @@ const Countries = () => {
 
     const region = countries.filter(item => item.region === e.target.value);
 
-    setSelectRegion(region)
-   
+    setRegion(region)
   }
   const regions = countries.map(country => country.region)
   const filteredRegions = regions.filter((country, index) => regions.indexOf(country) === index)
 
 
+
+
+  if (loading) return <h3 className={styles.loading}>Carregando...</h3>;
+  if (error) console.log(error);
+
   return (
     <main className={styles.main_container}>
-
       <div className={styles.filters}>
-      <input placeholder="Search for a country..." className={styles.input_search}  onChange={e =>setSearch(e.target.value)}/>
-      
+        <input
+          placeholder="Search for a country..."
+          className={styles.input_search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-      <select className={styles.select_filter} onChange={handleFilterCountriesByRegion}>
-         <option value="">Search by region</option>
-         {filteredRegions.map((country)=>(
-            <option key={country}>{country}</option>
+        <select className={styles.select_filter} onChange={handleFilterCountriesByRegion}>
+          <option value="">Search by region</option>
+          {filteredRegions.map((country, index) => (
+            <option key={index}>{country}</option>
           ))}
         </select>
       </div>
 
-      {search.length > 0  ? (
+      {search.length === 0  && region.length === 0 ? (
         <div className={styles.countries_container}>
-          {filteredCountries.map((country) => (
-            
-              <div className={styles.card_styles}>
-                <div className={styles.items}>
-                <img src={country.flag} alt="Country flag" />
-                <h3>{country.name}</h3>
-                <div className={styles.country_info}>
-                <p><strong>Region:</strong> {country.region}</p>
-                <p><strong>Population:</strong> {country.population}</p>
-                <p><strong>Capital:</strong> {country.capital}</p>
-                </div>
-                </div>  
+        {countries.map((country, index) => (
+          <div className={styles.card_styles} key={index}>
+            <div className={styles.items}>
+              <img src={country.flag} alt="Country flag" />
+              <h3>{country.name}</h3>
+              <div className={styles.country_info}>
+                <p>
+                  <strong>Region:</strong> {country.region}
+                </p>
+                <p>
+                  <strong>Population:</strong> {country.population}
+                </p>
+                <p>
+                  <strong>Capital:</strong> {country.capital}
+                </p>
               </div>
-           
-          ))}
-        </div>
+            </div>
+          </div>
+        ))}
+      </div>
+        
       ) : (
-        <div className={styles.countries_container}>
-          {countries.map((country) => (
-           
-              <div className={styles.card_styles}>
-                <div className={styles.items}>
+        search.length > 0 && region.length === 0 ? (
+          <div className={styles.countries_container}>
+          {filteredCountries.map((country, index) => (
+            <div className={styles.card_styles} key={index}>
+              <div className={styles.items}>
                 <img src={country.flag} alt="Country flag" />
                 <h3>{country.name}</h3>
                 <div className={styles.country_info}>
-                <p><strong>Region:</strong> {country.region}</p>
-                <p><strong>Population:</strong> {country.population}</p>
-                <p><strong>Capital:</strong> {country.capital}</p>
+                  <p>
+                    <strong>Region:</strong> {country.region}
+                  </p>
+                  <p>
+                    <strong>Population:</strong> {country.population}
+                  </p>
+                  <p>
+                    <strong>Capital:</strong> {country.capital}
+                  </p>
                 </div>
-                </div>  
               </div>
+            </div>
           ))}
         </div>
-      )}
-
-      </main>
+        ):  (
+          <div className={styles.countries_container}>
+          {region.map((country, index) => (
+            <div className={styles.card_styles} key={index}>
+              <div className={styles.items}>
+                <img src={country.flag} alt="Country flag" />
+                <h3>{country.name}</h3>
+                <div className={styles.country_info}>
+                  <p>
+                    <strong>Region:</strong> {country.region}
+                  </p>
+                  <p>
+                    <strong>Population:</strong> {country.population}
+                  </p>
+                  <p>
+                    <strong>Capital:</strong> {country.capital}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        ))}  
+    </main>
   );
 };
 
